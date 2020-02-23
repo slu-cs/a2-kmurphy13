819039481ebd0f876a31d1976dac47fc1b44c375
Kira <kiramurphy13@yahoo.com>
2020-02-23 18:08:23 -0500

    bugs

--- query.js
+++ query.js
@@ -6,10 +6,9 @@ const Voter = require('./schema');
 connect(); 
 
 const queries = [
-
-    Voter.find().where('zipcode').equals(13617).count(),
+    Voter.find().where('zipcode').equals(13617),
     Voter.find().where('first_name').equals("STARR"),
-    Voter.find({ "history": /GE16/i }).count(),
+    Voter.find({ "history": /GE16/i }),
     Voter.find().sort("-last_name").limit(1),
     Voter.distinct('zipcode')
   
@@ -18,9 +17,9 @@ const queries = [
   // Run the queries in parallel
   Promise.all(queries)
     .then(function(results) {
-      console.log('Number of registered voters in Canton: ', results[0][0]);
+      console.log('Number of registered voters in Canton: ', results[0].length);
       console.log('Full names of all registered voters whose first name is Starr: ', results[1].map(voter=> voter.first_name + " " +voter.last_name));
-      console.log("Number of people who voted in the 2016 General Election: ", results[2][0]);
+      console.log("Number of people who voted in the 2016 General Election: ", results[2].length);
       console.log("Final last name when sorted alphabetically: ", results[3][0].last_name);
       console.log("Number of distinct zipcodes: ", results[4].length);
       mongoose.connection.close();
