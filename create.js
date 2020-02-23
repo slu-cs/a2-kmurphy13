372cb8d58a33a1c713378c286f7e3839d0a2a6f0
Kira <kiramurphy13@yahoo.com>
2020-02-23 16:17:29 -0500

    new csv reading

--- create.js
+++ create.js
@@ -3,13 +3,30 @@
 const mongoose = require('mongoose');
 const connect = require('./db');
 const Voter = require('./schema');
+const fs = require('fs');
+const readline = require('readline');
+const file = readline.createInterface({
+  input: fs.createReadStream('voters.csv')
+});
 
 connect(); // To the database
 
-// Create some faculty
-var data = $.csv.toObjects(voters.csv);
-console.log(data)
 
+
+// Create an array of objects, so that each line of the file is represented by an object with three properties.
+const rows = [];
+file.on('line', function(line) {
+  const columns = line.split(',');
+  rows.push(new Voter({
+    first_name: columns[0],
+    last_name: columns[1],
+    zipcode: columns[2],
+    history: columns[3]
+    })
+  );
+});
+
+console.log(rows)
 // Reset the data
 /* mongoose.connection.dropDatabase()
   .then(() => harcourt.save())
